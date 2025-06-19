e8c156fbbb6a3d1e7e7878e0cdee1338e222cdeb
ktohda <ktohda0127@gmail.com>
2025-06-20 08:58:32 +0900

    タイルの型を追加

--- city-sim/src/types/grid.ts
+++ city-sim/src/types/grid.ts
@@ -2,3 +2,12 @@ export interface Position {
   x: number;
   y: number;
 }
+
+export interface GridSize {
+  width: number;
+  height: number;
+}
+
+export interface GridTile {
+  position: Position;
+}
