ed8f3aa650b32727930944f1b52af309278aa3c6
ktohda <ktohda0127@gmail.com>
2025-07-06 18:39:02 +0900

    インターフェース定義

---
+++ city-sim/src/components/InfoPanel.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { TbCoin, TbUsers, TbMoodHappy, TbCalendar } from 'react-icons/tb';
+import type { GameStats } from '../types/game';
+
+interface InfoPanelProps {
+  stats: GameStats;
+}
