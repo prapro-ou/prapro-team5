ce887f764811fbcef39da16c72776c37ee1f4bb2
ktohda <ktohda0127@gmail.com>
2025-07-23 23:55:01 +0900

    バリエーションインデックスに対応

--- city-sim/src/stores/FacilityStore.ts
+++ city-sim/src/stores/FacilityStore.ts
@@ -113,12 +113,19 @@ export const useFacilityStore = create<FacilityStore>((set, get) => ({
         occupiedTiles.push({ x: position.x + dx, y: position.y + dy });
       }
     }
+
+    let variantIndex = 0;
+    // 現在は画像が一枚しかないので、variantIndexは0固定
+    // if (facilityData.imgPaths && facilityData.imgPaths.length > 1) {
+    //   variantIndex = Math.floor(Math.random() * facilityData.imgPaths.length);
+    // }
     
     return {
       id: `${type}_${position.x}_${position.y}_${Date.now()}`,
       type,
       position,
-      occupiedTiles
+      occupiedTiles,
+      variantIndex,
     };
   }
 }));
