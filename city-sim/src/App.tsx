8f72ab69e2bd2cf815007ad4a7a019929f14633f
Yuichi Nakamura <grottaazzura 2023@gamil.com>
2025-07-26 14:59:21 +0900

    ボタンをトグル式に変更

--- city-sim/src/App.tsx
+++ city-sim/src/App.tsx
@@ -133,7 +133,7 @@ function App() {
       {/* 右上に設定ボタンと報酬ボタンを並べて配置 */}
       <div className="fixed top-4 right-4 flex gap-2 z-[1200]">
         <button
-          onClick={() => setShowRewardPanel(true)}
+          onClick={() => setShowRewardPanel(v => !v)}
           className="bg-yellow-400 hover:bg-yellow-500 text-black px-4 py-2 rounded-lg shadow-lg"
         >
           報酬
