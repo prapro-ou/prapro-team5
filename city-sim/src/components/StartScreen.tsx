965438c72a6f65f6af1705c2c46505969604f0b2
ktohda <ktohda0127@gmail.com>
2025-07-27 21:43:09 +0900

    ビル風の装飾を追加

--- city-sim/src/components/StartScreen.tsx
+++ city-sim/src/components/StartScreen.tsx
@@ -7,6 +7,23 @@ type Props = {
 
 const StartScreen: React.FC<Props> = ({ onStart, onShowSettings }) => (
   <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-900 z-[2000]">
+    {/* ビルの窓明かり */}
+    <div className="absolute left-0 bottom-0 flex flex-col">
+      <div className="flex flex-col">
+        {[...Array(5)].map((_, rowIndex) => (
+          <div key={`row-${rowIndex}`} className="flex">
+            {[...Array(3)].map((_, colIndex) => (
+              <div
+                key={`left-window-${rowIndex}-${colIndex}`}
+                className={`w-6 h-4 mx-1 mb-3 transition-all duration-300 ${
+                  Math.random() > 0.8 ? 'bg-gray-100/60' : 'bg-gray-700/40'
+                }`}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
     <div className="bg-white shadow-lg items-center rounded-lg">
       <img src="logo.svg" alt="Titile Logo" className="w-full h-36 object-contain p-4" />
     </div>
