435e71c6dfa9b53117e013a4bfee1347127f3637
ktohda <ktohda0127@gmail.com>
2025-07-27 20:38:04 +0900

    レイアウトを調整

--- city-sim/src/components/StartScreen.tsx
+++ city-sim/src/components/StartScreen.tsx
@@ -10,16 +10,16 @@ const StartScreen: React.FC<Props> = ({ onStart, onShowSettings }) => (
     <div className="bg-white shadow-lg items-center rounded-lg">
       <img src="logo.svg" alt="Titile Logo" className="w-full h-36 object-contain p-4" />
     </div>
-    <div className="flex flex-col items-center space-y-4 mt-16">
+    <div className="flex flex-col items-center space-y-4 mt-12">
       <button
         onClick={onStart}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-lg text-2xl shadow-lg"
+        className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-2 rounded-lg text-2xl shadow-lg w-48"
       >
         スタート
       </button>
       <button
         onClick={onShowSettings}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-lg text-2xl shadow-lg"
+        className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-2 rounded-lg text-2xl shadow-lg w-48"
       >
         設定
       </button>
