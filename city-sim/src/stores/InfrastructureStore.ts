e72c54ceac5af9f50b23a1f85f48d2f35c2a000b
ktohda <ktohda0127@gmail.com>
2025-08-05 16:13:46 +0900

    インフラストア定義

--- city-sim/src/stores/InfrastructureStore.ts
+++ city-sim/src/stores/InfrastructureStore.ts
@@ -6,3 +6,14 @@ export interface InfrastructureStatus {
   water: { demand: number; supply: number; balance: number };
   electricity: { demand: number; supply: number; balance: number };
 }
+
+interface InfrastructureStore {
+  status: InfrastructureStatus;
+  
+  // アクション
+  calculateInfrastructure: (facilities: Facility[]) => void;
+  getInfrastructureStatus: () => InfrastructureStatus;
+  getInfrastructureShortage: () => { water: number; electricity: number };
+  getInfrastructureSurplus: () => { water: number; electricity: number };
+}
+
