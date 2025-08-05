656d54dfba07756aace16b15c9a17d8dc699d125
ktohda <ktohda0127@gmail.com>
2025-08-05 16:13:20 +0900

    インフラ状態インターフェース定義

--- city-sim/src/stores/InfrastructureStore.ts
+++ city-sim/src/stores/InfrastructureStore.ts
@@ -1,3 +1,8 @@
 import { create } from 'zustand';
 import type { Facility } from '../types/facility';
 import { FACILITY_DATA } from '../types/facility';
+
+export interface InfrastructureStatus {
+  water: { demand: number; supply: number; balance: number };
+  electricity: { demand: number; supply: number; balance: number };
+}
