0de8890a01cdca9843bcd15d2a75a2276640accc
ktohda <ktohda0127@gmail.com>
2025-08-21 22:25:54 +0900

    支持システムストアのインターフェース定義

---
+++ city-sim/src/stores/SupportStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import type { 
+  FactionType, 
+  FactionSupport, 
+  SupportSystemState, 
+  MonthlySupportHistory,
+  YearlySupportHistory,
+  CityStateForSupport,
+  SupportCalculationResult,
+  SupportLevelEffect
+} from '../types/support';
+import { FACTION_DATA, SUPPORT_LEVEL_EFFECTS } from '../types/support';
+import { saveLoadRegistry } from './SaveLoadRegistry';
+
+// SupportStoreのインターフェース
+interface SupportStore {
+  // 状態
+  state: SupportSystemState;
+  
+  // 基本アクション
+  initializeSupportSystem: () => void;
+  getFactionSupport: (factionType: FactionType) => FactionSupport | undefined;
+  getAllFactionSupports: () => FactionSupport[];
+  
+  // 支持率の更新
+  updateFactionSupport: (factionType: FactionType, newRating: number) => void;
+  updateAllFactionSupports: (newRatings: Record<FactionType, number>) => void;
+  
+  // 支持率の計算
+  calculateSupportRatings: (cityState: CityStateForSupport) => Record<FactionType, number>;
+  calculateFactionSupport: (factionType: FactionType, cityState: CityStateForSupport) => SupportCalculationResult;
+  
+  // セーブ・ロード機能
+  saveState: () => any;
+  loadState: (savedState: any) => void;
+  resetToInitial: () => void;
+}
